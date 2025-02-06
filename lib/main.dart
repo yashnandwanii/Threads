5379f2af860a2dcb68c69b34a53ccb7788aa3787
yashnandwanii <yashnandwani47@gmail.com>
2025-02-06 15:07:16 +0530

    Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:lottie/lottie.dart';
 import 'package:threads/routes/routes.dart';
-import 'package:threads/routes/routes_name.dart';
 import 'package:threads/services/storage_services.dart';
 import 'package:threads/services/supabase_services.dart';
 import 'package:threads/themes/theme.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:threads/views/auth/login.dart';
+import 'package:threads/views/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,9 +30,23 @@ class MainApp extends StatelessWidget {
       title: "Threads",
       theme: theme,
       getPages: Routes.pages,
-      initialRoute: StorageServices.userSession != null
-          ? RoutesName.home
-          : RoutesName.login,
+      home: const SplashScreen(), // Set SplashScreen as the initial screen
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splashIconSize: 300,
+      splash: Lottie.asset('assets/animations/loading.json'),
+      nextScreen: StorageServices.userSession != null ? Home() : LoginPage(),
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      backgroundColor: Colors.black,
     );
   }
 }
